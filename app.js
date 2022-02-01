a78b64cd1824e87117e5175bf238a21709393fdc
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-02-01 11:51:01 +0200

    Exported

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+// const makeCamelCase = require('./functions');
+// const snakeCase = require('./functions');
+// const kebabCase = require('./functions');
+
+// const view = makeCamelCase('Vienas zodis');
+// console.log(view);
 console.log('Veikia');
